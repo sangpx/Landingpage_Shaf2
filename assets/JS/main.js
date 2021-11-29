23ad41ffae81128e0f2c1b00904a943ed04ced83
pham sang <psang4845@gamil.com>
2021-11-30 01:13:51 +0700

    first commit

---
+++ assets/JS/main.js
@@ -0,0 +1,17 @@
+// cu phap co ban
+
+
+
+// $(document).ready(
+//      function () {
+//           $('#about').waypoint(
+//                function (direction) {
+//                     if(direction == 'down') {
+//                          $('nav').addClass( 'sticky');
+//                     }
+//                } , {
+//                     offset: '100px'
+//                } 
+//           ) 
+//      }
+// )
